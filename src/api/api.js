cda6d301e0e2a5c61d8da1fdda797d7122b25cb6
Treee <jonathon.lindemann@gmail.com>
2020-08-13 10:20:22 -0600

    adding more methods in allowed headers

--- src/api/api.js
+++ src/api/api.js
@@ -17,7 +17,7 @@ function _startServer(port, dbConnection) {
         if (allowedOrigins.indexOf(origin) > -1) {
             res.setHeader('Access-Control-Allow-Origin', origin);
         }
-        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, DELETE');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.header('Access-Control-Allow-Credentials', true);
         return next();
