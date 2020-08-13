a436dbaf19d27fccf91b80d05ca1f00ef73c653a
Treee <jonathon.lindemann@gmail.com>
2020-08-13 14:04:39 -0600

    adding tournament

--- src/api/api.js
+++ src/api/api.js
@@ -27,87 +27,14 @@ function _startServer(port, dbConnection) {
 
     const playerRouter = require('./player-routes')(express, dbConnection);
     app.use('/api/players', playerRouter);
-    // app.get('/', (req, res) => res.send('Hello World!'), errorHandler);
+
+    const tournamentRouter = require('./tournament-routes')(express, dbConnection);
+    app.use('/api/tournaments', tournamentRouter);
 
 
     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 
-    // const apiRouter = express.Router();
-
-    // apiRouter.route('/api/playerz/:id')
-    //     .post((req, res) => {
-    //         console.log('delete /api/player', req.body);
-    //         console.log('delete /api/player', req.params.playerId);
-    //         const playerId = req.params.id || req.body.playerId;
-    //         dbConnection.deletePlayer(playerId).then(() => {
-    //             console.log('deletePlayer');
-    //             res.status(200).json();
-    //         });
-    //     }, errorHandler);
-
-    // apiRouter.route('/api/players').get((req, res) => {
-    //     // console.log('router /api/players');
-    //     dbConnection.getAllPlayers().then((players) => {
-    //         console.log('players', players);
-    //         res.status(200).json(players);
-    //     });
-    // }, errorHandler);
-
-    // apiRouter.route('/api/player')
-    //     .post((req, res) => {
-    //         console.log('post /api/player', req.body);
-    //         dbConnection.createPlayer(req.body.name, req.body.team).then((newPlayer) => {
-    //             console.log('newPlayer', newPlayer);
-    //             res.status(200).json(newPlayer);
-    //         });
-    //     }, errorHandler);
-
-    // app.get('/api/players', (req, res) => {
-    //     // console.log('router /api/players');
-    //     dbConnection.getAllPlayers().then((players) => {
-    //         console.log('players', players);
-    //         res.status(200).json(players);
-    //     });
-    // }, errorHandler);
-
-    // app.post('/api/player', (req, res) => {
-    //     console.log('post /api/player', req.body);
-    //     dbConnection.createPlayer(req.body.name, req.body.team).then((newPlayer) => {
-    //         console.log('newPlayer', newPlayer);
-    //         res.status(200).json(newPlayer);
-    //     });
-    // }, errorHandler);
-
-    // app.delete('/api/player', (req, res) => {
-    //     console.log('delete /api/player', req.body);
-    //     console.log('delete /api/player', req.params.playerId);
-    //     const playerId = req.params.playerId || req.body;
-    //     dbConnection.deletePlayer(playerId).then(() => {
-    //         console.log('deletePlayer');
-    //         res.status(200).json();
-    //     });
-    // }, errorHandler);
-
-    // apiRouter.route('/api/tournaments')
-    //     .get((req, res) => {
-    //         // console.log('router /api/tournaments');
-    //         dbConnection.getAllTournaments().then((tournaments) => {
-    //             // console.log('tournaments', tournaments);
-    //             res.status(200).json(tournaments);
-    //         });
-    //     }, errorHandler);
-
-
-    // app.use('/', apiRouter);
-    // // app.get('/', (req, res) => res.send('Hello World!'), errorHandler);
-
-
-    // function errorHandler(error) {
-    //     console.log('Error', error);
-    // }
-
-    // app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 }
 
 module.exports = {
--- src/api/tournament-routes.js
+++ src/api/tournament-routes.js
@@ -1,6 +1,18 @@
 module.exports = (express) => {
     const tournamentRouter = express.Router();
 
+    tournamentRouter.route('/all').get((req, res) => {
+        // console.log('router /api/tournaments');
+        dbConnection.getAllTournaments().then((tournaments) => {
+            // console.log('tournaments', tournaments);
+            res.status(200).json(tournaments);
+        });
+    }, errorHandler);
+
+
+    function errorHandler(error) {
+        console.log('Error', error);
+    }
 
     return tournamentRouter;
 }
