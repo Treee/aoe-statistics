e5e478141645fe3a335f34b50dc254c6c4319358
Treee <jonathon.lindemann@gmail.com>
2020-08-28 14:36:25 -0600

    removing commented code

--- src/api/twitch-webhook-routes.js
+++ src/api/twitch-webhook-routes.js
@@ -1,38 +1,13 @@
 module.exports = (express, dbConnection) => {
-  const playerRouter = express.Router();
+  const twitchWebhook = express.Router();
 
-  playerRouter.route('/callback').get((req, res) => {
-    console.log("stuff", req);
+  twitchWebhook.route('/callback').get((req, res) => {
     res.status(200).json({});     
   }, errorHandler);
-
-  // playerRouter.route('/:id')
-  //     .get((req, res) => {
-  //         console.log('get by id', req.params.id);
-  //         dbConnection.getPlayerById(req.params.id).then((players) => {
-  //             console.log('players', players);
-  //             res.status(200).json(players);
-  //         });
-  //     }, errorHandler)
-  //     .post((req, res) => {
-  //         console.log(`player body: ${req.body} params: ${req.params}`);
-  //         dbConnection.createPlayer(req.body.name, req.body.team).then((newPlayer) => {
-  //             console.log('newPlayer', newPlayer);
-  //             res.status(200).json(newPlayer);
-  //         });
-  //     }, errorHandler)
-  //     .delete((req, res) => {
-  //         console.log(`delete player body: ${req.body} params: ${req.params}`);
-  //         const playerId = req.params.id || req.body.id;
-  //         dbConnection.deletePlayer(playerId).then(() => {
-  //             console.log('deletePlayer');
-  //             res.status(200).json({});
-  //         });
-  //     }, errorHandler);
 
   function errorHandler(error) {
       console.log('Error', error);
   }
 
-  return playerRouter;
+  return twitchWebhook;
 }
