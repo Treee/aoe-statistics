264a719665a8d956ec9fb1a444364720bc908cf7
Treee <jonathon.lindemann@gmail.com>
2020-08-28 15:43:33 -0600

    responding with hub challenge

--- src/api/twitch-webhook-routes.js
+++ src/api/twitch-webhook-routes.js
@@ -1,10 +1,10 @@
 module.exports = (express, dbConnection) => {
   const twitchWebhook = express.Router();
 
-  twitchWebhook.route('/callback').get((req, res) => {
+  twitchWebhook.route('/follows').get((req, res) => {
     console.log(req);
     console.log(req.body);
-    res.status(200).send("");     
+    res.status(200).send(req.query.hub.challenge);     
   }, errorHandler);
 
   function errorHandler(error) {
