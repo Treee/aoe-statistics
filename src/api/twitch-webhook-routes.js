66078da2444c25d9685d0856ad23fdde85507a69
Treee <jonathon.lindemann@gmail.com>
2020-08-28 17:08:10 -0600

    adding post

--- src/api/twitch-webhook-routes.js
+++ src/api/twitch-webhook-routes.js
@@ -2,14 +2,18 @@ module.exports = (express, dbConnection) => {
   const twitchWebhook = express.Router();
 
   twitchWebhook.route('/follows').get((req, res) => {
-    // in the future check the hub challenge to make sure it is correct
-    // sha256(secret, notification_bytes).
-    console.log(req);
-    res.status(200).send(req.query['hub.challenge']);     
-  }, errorHandler);
+      // in the future check the hub challenge to make sure it is correct
+      // sha256(secret, notification_bytes).
+      const x = req.query['hub.challenge'];
+      console.log(x);
+      res.status(200).send(req.query['hub.challenge']);
+    }, errorHandler)
+    .post((req, res) => {
+      res.status(200).send(req.body);
+    }, errorHandler);
 
   function errorHandler(error) {
-      console.log('Error', error);
+    console.log('Error', error);
   }
 
   return twitchWebhook;
