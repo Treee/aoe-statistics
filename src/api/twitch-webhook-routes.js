fcbb96ff33e92551e1f56b3941640bf0c14ec238
Treee <jonathon.lindemann@gmail.com>
2020-08-28 16:04:27 -0600

    adding some debug

--- src/api/twitch-webhook-routes.js
+++ src/api/twitch-webhook-routes.js
@@ -2,8 +2,9 @@ module.exports = (express, dbConnection) => {
   const twitchWebhook = express.Router();
 
   twitchWebhook.route('/follows').get((req, res) => {
-    console.log(req);
-    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
+    console.log(req.url);
     res.status(200).send(req.query.hub.challenge);     
   }, errorHandler);
 
