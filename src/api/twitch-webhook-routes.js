e3fa494c937d75351ace91172d4f7fc7317b85ea
Treee <jonathon.lindemann@gmail.com>
2020-08-28 18:39:08 -0600

    adding console log in post response

--- src/api/twitch-webhook-routes.js
+++ src/api/twitch-webhook-routes.js
@@ -5,11 +5,11 @@ module.exports = (express, dbConnection) => {
       // in the future check the hub challenge to make sure it is correct
       // sha256(secret, notification_bytes).
       const hubChallenge = req.query['hub.challenge'];
-      console.log('hub challenge', hubChallenge);
       res.setHeader('content-type', 'text/plain');
       res.status(200).send(JSON.stringify(hubChallenge));
     }, errorHandler)
     .post((req, res) => {
+      console.log('post follow request', req.body);
       res.status(200).send(req.body);
     }, errorHandler);
 
